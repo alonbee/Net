ca3dafaf4ff56091c9b4519ccf90d692f5ac2a95
Allenwu <chimian.wu@gmail.com>
2016-04-27 19:24:45 -0400

    add server code

--- server/srt_server.c
+++ server/srt_server.c
@@ -10,6 +10,8 @@
 //
 
 #include "srt_server.h"
+svr_tcb_t* svr_tcb_table[MAX_TRANSPORT_CONNECTIONS];  /* number of allowed client connections*/
+int tcp_socknum = -1;  // Socket number from TCP protocal
 
 //
 //
@@ -38,6 +40,20 @@
 //
 void srt_server_init(int conn)
 {
+	// memset(&svr_tcb_table,NULL, sizeof(svr_tcb_table));
+  
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; ++i)
+  {
+  	svr_tcb_table[i] = NULL;
+  }
+  tcp_socknum = conn;
+  pthread_t pid;
+  int err = pthread_create(&pid, NULL, seghandler, (void *)NULL);
+  if (err < 0) 
+    printf("Failed to init server.\n");
+   else
+   	printf("Successful to init server\n");
+	
   return;
 }
 
@@ -54,7 +70,30 @@ void srt_server_init(int conn)
 //
 int srt_server_sock(unsigned int port)
 {
-  return 0;
+  int sockfd = 0;
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; ++i) {
+    if (svr_tcb_table[i] == NULL) {
+      sockfd = i;
+      // Initilize tcb 
+      svr_tcb_t *tp = (svr_tcb_t*) malloc(sizeof(svr_tcb_t));
+      svr_tcb_table[i] = tp;
+      tp -> svr_portNum = port;   /*Set original port*/
+      tp -> state = CLOSED;
+      printf("Sevrer: TCB socket=%d created successfully\n",sockfd);
+      return sockfd;
+    }
+  }
+  printf("Sevrer: SOCK: Can't create for client_port = %d\n", port);
+  return -1;
+}
+
+// Get tcb for server by socketfd
+svr_tcb_t* gettcb(int sockfd) {
+  svr_tcb_t* tp; 
+  tp = svr_tcb_table[sockfd];
+  if (!tp)
+    printf("Sevrer: Error in getting svr_tcb_t for sockefd = %d\n", sockfd);
+  return tp;
 }
 
 
@@ -68,7 +107,31 @@ int srt_server_sock(unsigned int port)
 //
 int srt_server_accept(int sockfd)
 {
-  return 0;
+  svr_tcb_t*tp = malloc(sizeof(svr_tcb_t));
+
+  tp = gettcb(sockfd);
+  if (!tp) {
+    printf("Sevrer: Connect:Can't get svr_tcb_t sockfd = %d\n", sockfd);
+    return -1;
+  }
+  printf("Server: Ready to accept\n");
+  switch (tp -> state) {
+    case CLOSED:
+    	tp -> state = LISTENING;
+    	while(tp ->state != CONNECTED) {
+
+    	}	
+    	printf("Server: server sockfd=%d accept successful\n", sockfd);
+    	return 1;
+    case LISTENING:
+      return -1;
+    case CONNECTED:
+      return -1;
+    case CLOSEWAIT:
+      return -1;
+    default:
+      return -1;
+    }
 }
 
 
@@ -95,9 +158,37 @@ int srt_server_recv(int sockfd, void* buf, unsigned int length)
 //
 int srt_server_close(int sockfd)
 {
-  return 0;
+  // TODO: Free tcb entry
+  svr_tcb_t *tp;
+  tp = gettcb(sockfd);
+  if (!tp) {
+    printf("Server: Can't close sockfd = %d\n", sockfd);
+      return -1;
+  }
+  switch(tp -> state) {
+    case CLOSED:
+      free(svr_tcb_table[sockfd]);
+      svr_tcb_table[sockfd] = NULL;
+      return 1;
+    case LISTENING:
+      return -1;
+    case CONNECTED:
+      return -1;
+    case CLOSEWAIT:
+      return -1;
+    default:
+      return -1;
+  }
 }
 
+
+void* closewait(void* tcb) {
+  svr_tcb_t* tp = (svr_tcb_t*)tcb;
+  sleep(CLOSEWAIT_TIME);
+
+  tp->state = CLOSED;
+  pthread_exit(NULL);
+}
 
 // This is a thread  started by srt_server_init(). It handles all the incoming 
 // segments from the client. The design of seghanlder is an infinite loop that calls snp_recvseg(). If
@@ -109,6 +200,124 @@ int srt_server_close(int sockfd)
 //
 void* seghandler(void* arg)
 {
-  return 0;
+  seg_t* seg = malloc(sizeof(seg_t));
+  seg_t* ack = malloc(sizeof(seg_t));
+  // memset(&seg, 0, sizeof(seg));
+  svr_tcb_t* tp = malloc(sizeof(svr_tcb_t));
+
+  while (1) {
+    // Find the right server
+    tp = NULL;
+    if (snp_recvseg(tcp_socknum,seg) != 1) {
+      close(tcp_socknum);
+          pthread_exit(NULL);
+    }
+
+    // Get socketfd by port
+    int sockfd = -1;
+    for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; ++i)
+      {
+        if (svr_tcb_table[i] != NULL && svr_tcb_table[i] -> svr_portNum == seg->header.dest_port) {
+          // Get the right tcb based on dest port
+            sockfd = i; /*get socket number in srt for client port*/
+            // tp -> client_port = seg.header.src_port;
+            tp = svr_tcb_table[i];
+          }
+        }
+
+      if (!tp){
+      printf("Server: Can't get server_tcp for the seg\n");
+      continue;
+        }
+
+      printf("seghandler sockfd=%d , state=%d \n", sockfd, tp->state);
+    switch(tp->state) {
+      case CLOSED:
+          printf("Server: sockfd=%d received seg in CLOSED\n", sockfd);
+        break;
+      case LISTENING:
+        if(seg->header.type == SYN) {
+          tp->client_portNum = seg->header.src_port;
+          tp->state=CONNECTED;
+          printf("Server:sockfd = %d Got SYN from client\n", sockfd);
+          
+          // memset(&ack,0, sizeof(ack));
+          ack->header.type = SYNACK;
+          ack->header.src_port = tp -> svr_portNum;
+          ack->header.dest_port = tp -> client_portNum;
+          ack->header.length = 0;
+          snp_sendseg(tcp_socknum, ack);
+          printf("Server:sockfd = %d Sent SYNACK to client\n", sockfd);
+        }
+        else
+          printf("Server: Listening received SYN\n");
+        break;
+      case CONNECTED: 
+      {
+        switch (seg->header.type) {
+          printf("Server: tp %d: receive %d\n", sockfd, seg->header.type);
+          case SYN:
+          {
+            // send SYNACK
+            // seg_t ack;
+            // memset(&ack,0, sizeof(ack));
+            ack->header.type = SYNACK;
+            ack->header.src_port = tp -> svr_portNum;
+            ack->header.dest_port = tp -> client_portNum;
+            ack->header.length = 0;
+            snp_sendseg(tcp_socknum, ack);
+
+            break;            
+          }
+          case FIN:
+          {
+            time_t timer = time(NULL);
+            printf("Server: tp %d: receive FIN %s", sockfd, ctime(&timer));
+
+            // send FINACK
+            // seg_t ack;
+            // memset(&ack,0, sizeof(ack));
+            ack->header.type = FINACK;
+            ack->header.src_port = tp -> svr_portNum;
+            ack->header.dest_port = tp -> client_portNum;
+            ack->header.length = 0;
+            snp_sendseg(tcp_socknum, ack);
+
+            tp->state = CLOSEWAIT;
+
+            // start a thread for CLOSE_WAIT_TIMEOUT
+            pthread_t closetimer;
+            pthread_create(&closetimer,NULL,closewait, (void*)tp);
+
+            break;                    
+          }
+          default:
+            break;
+          }
+        break;
+      }
+      case CLOSEWAIT:
+        // receive FIN
+        if (seg->header.type == FIN) {
+
+          // send FINACK
+          // seg_t ack;
+          // memset(&ack,0, sizeof(ack));
+          ack->header.type = FINACK;
+          ack->header.src_port = tp -> svr_portNum;
+          ack->header.dest_port = tp -> client_portNum;
+          ack->header.length = 0;
+          snp_sendseg(tcp_socknum, ack);
+          printf("Server: sent FINACK in closewait\n");
+          }
+        else {
+          printf("Server: Received not fin in CLOSEWAIT\n");
+        }
+        break;
+      default:
+        printf("Server: sockfd= %d: wrong state\n", sockfd);
+        break;
+    }
+  }
 }
 
